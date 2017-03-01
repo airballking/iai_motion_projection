b9076ccad7ed9cfc2cff704aa024fa120a91de36
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2017-03-01 16:49:13 +0100

    Finished first prototype of a projection.

--- src/projection_manager.py
+++ src/projection_manager.py
@@ -4,6 +4,7 @@
 
 import rospy
 from iai_naive_kinematics_sim.msg import ProjectionClock
+from iai_naive_kinematics_sim.srv import SetJointState
 from iai_motion_projection.srv import TriggerProjection
 from std_msgs.msg import Header
 
@@ -14,21 +15,23 @@ class ProjectionManager:
     def __init__(self):
         rospy.init_node('projection_manager')
         self.iteration_finished = False
-        self.last_clock_msg = ProjectionClock()  # TODO: switch to token
+        self.last_clock_msg = ProjectionClock()
         self.clock_pub = rospy.Publisher('~projection_clock', ProjectionClock, queue_size=10, tcp_nodelay=True)
-        self.server = rospy.Service('~trigger_projection', TriggerProjection, self.projection_callback)
         self.ack_sub = rospy.Subscriber('~iteration_completed', Header, self.iteration_callback, queue_size=10, tcp_nodelay=True)
+        self.set_joint_state = rospy.ServiceProxy('~set_joint_states', SetJointState, persistent=True)
+        self.set_joint_state.wait_for_service(1.0)
+        self.trigger_server = rospy.Service('~trigger_projection', TriggerProjection, self.projection_callback)
         # TODO: subscribe to topic with collision states
 
     def projection_callback(self, req):
         rospy.loginfo('received a trigger to start a projection')
-        # TODO: set initial joint state
+        self.set_joint_state(req.state)
         self.last_clock_msg = ProjectionClock(rospy.Time(0.0), req.period)
         self.iteration_finished = False
         for i in range(req.iterations):
             self.clock_pub.publish(self.last_clock_msg)
 
-            r = rospy.Rate(100)
+            r = rospy.Rate(20000)
             while (not self.iteration_finished):
                 r.sleep()
 
@@ -38,13 +41,11 @@ class ProjectionManager:
         return True
 
     def iteration_callback(self, data):
-        rospy.loginfo("Got a message")
-        # TODO: check for token instead
         if (self.last_clock_msg.now == data.stamp):
             self.iteration_finished = True
         else:
             self.iteration_finished = False
-            # TODO: raise an error
+            rospy.logerr("Received an invalid message over topic '~iteration_completed'.")
 
 
 # TODO: document main
@@ -57,5 +58,5 @@ def main():
 if __name__ == '__main__':
     try:
         main()
-    except rospy.ROSInterruptException:
-        pass
+    except rospy.ROSException as exc:
+        rospy.loginfo("Died on an exception: " + str(exc))
